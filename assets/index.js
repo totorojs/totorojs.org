78796f931fff5f175378db45da8d6445ce821191
fool2fish <fool2fish@gmail.com>
2014-02-27 19:35:49 +0800

    A new homepage for v0.5.0.

---
+++ assets/index.js
@@ -0,0 +1,16 @@
+(function() {
+  $.ajax({
+    url: 'http://server.totorojs.org:9997/overview',
+    dataType: "jsonp",
+    success: function (rt) {
+      if (rt.err) return
+
+      var data = rt.data
+      var content = '<table><tbody>' +
+        '<tr><th>Total:</th><td>' + data.orderCount + ' / ' + data.laborCount + '</td></tr>' +
+        '<tr><th>Last week:</th><td>' + data.recentWeekOrderCount + ' / ' + data.recentWeekLaborCount + '</td></tr>' +
+        '</tbody></table>'
+      $('#J_Statistic').html(content)
+    }
+  })
+})()
